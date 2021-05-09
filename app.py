785f0cbd1a2c316e3a42a9f5acc66b8ee06ae2aa
yattinda <yattinda@gmail.com>
2021-05-09 21:56:58 +0900

    get footcount

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ import json
 import os
 import random
 from datetime import datetime, date, timedelta
+import fitbit
+import key.py
 
 def puttweet():
-    twitter = OAuth1Session(os.environ["TW_CONSUMER_KEY"],
-              os.environ["TW_CONSUMER_SECRET"],
-              os.environ["TW_ACCESS_TOKEN_KEY"],
-              os.environ["TW_ACCESS_TOKEN_SECRET"])
-
-    fitbit = OAuth1Session(os.environ["FB_CLIENT_ID"],
-              os.environ["FB_CLIENT_SECRET"],
-              os.environ["FB_ACCESS_TOKEN"],
-              os.environ["FB_REFRESH_TOKEN"])
-
     yestaday = datetime.strftime(datetime.today() - timedelta(days=1), '%Y-%m-%d')
-    tweets = []
-    randomtweet = tweets[random.randrange(len(tweets))]
-    params = {"status": randomtweet}
-    req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
+
+    steps = fitbit_client.activities(date=yestaday)["summary"]["steps"]
+    # print(steps)
+    #
+    # tweets = []
+    # randomtweet = tweets[random.randrange(len(tweets))]
+    # params = {"status": randomtweet}
+    # req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params = params)
+
+if __name__ == '__main__':
+    puttweet()
