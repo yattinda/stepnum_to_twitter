d289301cb094323d9a2852f3bd566a71535e4452
yattinda <yattinda@gmail.com>
2021-05-10 17:28:49 +0900

    Save oneweek data as png

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ import json
 from datetime import datetime, date, timedelta
 import fitbit
 from key import fitbit_client
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-def puttweet():
-    yestaday = datetime.strftime(datetime.today() - timedelta(days=1), '%Y-%m-%d')
 
-    steps = fitbit_client.activities(date=yestaday)["summary"]["steps"]
-    # print(steps)
+def plotstep():
+    week = []
+    step = []
+    for i in range(6):
+        week.append(datetime.strftime(datetime.today() - timedelta(days=i), '%Y-%m-%d'))
+
+    for j in range(6):
+        step.append(fitbit_client.activities(date=week[j])["summary"]["steps"])
+
+    week.reverse()
+    step.reverse()
+
+    plt.plot(week, step)
+    plt.savefig('graph/{}.png'.format(datetime.strftime(datetime.today(), '%Y-%m-%d')))
+
 if __name__ == '__main__':
-    puttweet()
+    plotstep()
