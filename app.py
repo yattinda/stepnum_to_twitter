7001e203ce216b5876eb1d3bca9c310b8d33b236
yattinda <yattinda@gmail.com>
2021-05-10 18:27:37 +0900

    Add calories data but cant take heartrate

--- app.py
+++ app.py
@@ -10,19 +10,25 @@ import matplotlib.pyplot as plt
 
 
 def plotstep():
+    #print(fitbit_client.activities(date="2021-05-02"))
+    type = int(input("数字を入力\n1:歩数\n2:消費したカロリー\n>>"))
     week = []
-    step = []
     for i in range(6):
         week.append(datetime.strftime(datetime.today() - timedelta(days=i), '%Y-%m-%d'))
+    week.reverse()
+
+    if type == 1:
+        word = "steps"
+    elif type == 2:
+        word = "activityCalories"
+
+    data = []
 
     for j in range(6):
-        step.append(fitbit_client.activities(date=week[j])["summary"]["steps"])
+        data.append(fitbit_client.activities(date=week[j])["summary"][word])
 
-    week.reverse()
-    step.reverse()
-
-    plt.plot(week, step)
-    plt.savefig('graph/{}.png'.format(datetime.strftime(datetime.today(), '%Y-%m-%d')))
+    plt.plot(week, data)
+    plt.savefig('graph/{}{}.png'.format(word, datetime.strftime(datetime.today(), '%Y-%m-%d')))
 
 if __name__ == '__main__':
     plotstep()
